48bbe83b9aa4bf2b0aeba24459887a3835f2eae4
Fellipe Utaka <fellipeutaka@gmail.com>
2024-09-13 01:06:46 -0300

    feat: add logs to biome init

--- src/commands/biome/init.ts
+++ src/commands/biome/init.ts
@@ -1,39 +1,48 @@
 import fs from "node:fs";
 import { Command } from "commander";
 import { handleError } from "~/utils/handle-error";
+import { logger } from "~/utils/logger";
 import { Err, Ok } from "~/utils/result";
 import { initBiome } from "./helpers/init-biome";
 
 export interface InitOptions {
-  cwd: string;
+	cwd: string;
 }
 
 export const init = new Command()
-  .name("init")
-  .description("Init Biome")
-  .option(
-    "-c, --cwd <cwd>",
-    "the working directory. defaults to the current directory.",
-    process.cwd()
-  )
-  .action(async (options: InitOptions) => {
-    const optionsResult = parseOptions(options);
+	.name("init")
+	.description("Init Biome")
+	.option(
+		"-c, --cwd <cwd>",
+		"the working directory. defaults to the current directory.",
+		process.cwd(),
+	)
+	.action(async (options: InitOptions) => {
+		const optionsResult = parseOptions(options);
 
-    if (optionsResult.isErr()) {
-      handleError(optionsResult.error);
-    }
+		if (optionsResult.isErr()) {
+			handleError(optionsResult.error);
+		}
 
-    const initResult = await initBiome(optionsResult.value);
+		const initResult = await initBiome(optionsResult.value);
 
-    if (initResult.isErr()) {
-      handleError(initResult.error);
-    }
-  });
+		if (initResult.isErr()) {
+			handleError(initResult.error);
+		}
+
+		logger.break();
+		logger.success("Biome file created.");
+		logger.info(
+			"Check out the Biome documentation to learn more about how to use it.",
+		);
+		logger.info("https://biomejs.dev/guides/getting-started/#usage");
+		logger.break();
+	});
 
 function parseOptions(options: InitOptions) {
-  if (!fs.existsSync(options.cwd)) {
-    return new Err(`The directory ${options.cwd} does not exist.`);
-  }
+	if (!fs.existsSync(options.cwd)) {
+		return new Err(`The directory ${options.cwd} does not exist.`);
+	}
 
-  return new Ok(options);
+	return new Ok(options);
 }
