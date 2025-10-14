8210a3d334e3049951237bb3485a0491b61e387e
Fellipe Utaka <fellipeutaka@gmail.com>
2025-10-14 09:13:09 -0300

    chore: bump deps

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
@@ -7,9 +9,6 @@ const config = {
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
   devIndicators: false,
   // biome-ignore lint/suspicious/useAwait: This is a Next.js API
